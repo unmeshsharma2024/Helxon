22f27953e20f8587e2af2ca900e62de4f5a15c64
unmeshsharma2024 <unmeshsharma.25@gmail.com>
2025-11-11 00:33:11 -0800

    added email config

--- src/config/emailConfig.js
+++ src/config/emailConfig.js
@@ -1,6 +1,6 @@
 export const emailConfig = {
-  publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "",
-  serviceId: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "",
-  templateId: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "",
+  publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "ZkI2QRmzCjyocO8Sy",
+  serviceId: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || "service_oo2iooi",
+  templateId: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || "template_fl3pfqg",
 };
 
